c469e725c4332d7570e6bef72cd8ad53b2643276
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-25 02:24:59 +0800

    feat: get class of new window

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use hyprland::event_listener::EventListener;
 fn main() -> hyprland::Result<()> {
     let mut event_listener = EventListener::new();
 
-    event_listener.add_active_window_changed_handler(|data| println!("{data:#?}"));
+    event_listener.add_active_window_changed_handler(|data| {
+        if let Some(v) = data {
+            println!("{:?}", v.class)
+        }
+    });
     event_listener.start_listener()?;
 
     Ok(())
