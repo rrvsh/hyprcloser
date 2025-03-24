fe85b90503b27079d168178972f63ff41f17f2d6
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-25 02:36:00 +0800

    feat: get prev and current window

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use hyprland::event_listener::EventListener;
+use std::cell::RefCell;
 
 fn main() -> hyprland::Result<()> {
     let mut event_listener = EventListener::new();
+    let prev_window = RefCell::new(String::new());
+    let curr_window = RefCell::new(String::new());
 
-    event_listener.add_active_window_changed_handler(|data| {
-        if let Some(v) = data {
-            println!("{:?}", v.class)
+    event_listener.add_active_window_changed_handler(move |data| {
+        if let Some(window) = data {
+            let mut prev = prev_window.borrow_mut();
+            let mut curr = curr_window.borrow_mut();
+            *prev = curr.clone();
+            *curr = window.class;
+            println!("---");
+            println!("prev window: {:?}", *prev);
+            println!("curr window: {:?}", *curr);
         }
     });
     event_listener.start_listener()?;
