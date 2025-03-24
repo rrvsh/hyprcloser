d646ba18f8ec72d5acc60f1e1a2c5f89c07550cd
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-25 02:10:23 +0800

    feat: add event listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use hyprland::event_listener::EventListener;
+
+fn main() -> hyprland::Result<()> {
+    let mut event_listener = EventListener::new();
+
+    event_listener.add_active_window_changed_handler(|data| println!("{data:#?}"));
+    event_listener.start_listener()?;
+
+    Ok(())
 }
